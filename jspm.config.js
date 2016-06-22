c437f19d58d0211b929e0111ab65c82d14f7741f
Sven Reglitzki <sven.reglitzki@flavia-it.de>
2016-06-22 20:26:25 +0200

    Rearrange packages and update to angular rc.3

--- jspm.config.js
+++ jspm.config.js
@@ -33,7 +33,12 @@ SystemJS.config({
       'plugin-babel': 'npm:systemjs-plugin-babel@0.0.12',
       'scss': 'github:KevCJones/plugin-scss@0.2.11',
       'systemjs-hot-reloader': 'github:capaj/systemjs-hot-reloader@0.5.9',
-      'tty': 'github:jspm/nodelibs-tty@0.2.0-alpha'
+      'tty': 'github:jspm/nodelibs-tty@0.2.0-alpha',
+      'os': 'github:jspm/nodelibs-os@0.2.0-alpha',
+      'plugin-typescript': 'github:frankwallis/plugin-typescript@4.0.16',
+      'assert': 'github:jspm/nodelibs-assert@0.2.0-alpha',
+      'path': 'github:jspm/nodelibs-path@0.2.0-alpha',
+      'util': 'github:jspm/nodelibs-util@0.2.0-alpha'
     },
     'packages': {
       'github:KevCJones/plugin-scss@0.2.11': {
@@ -102,8 +107,21 @@ SystemJS.config({
         'map': {
           'caniuse-db': 'npm:caniuse-db@1.0.30000485'
         }
+      },
+      'github:jspm/nodelibs-os@0.2.0-alpha': {
+        'map': {
+          'os-browserify': 'npm:os-browserify@0.2.1'
+        }
+      },
+      'github:frankwallis/plugin-typescript@4.0.16': {
+        'map': {
+          'typescript': 'npm:typescript@1.8.10'
+        }
       }
     }
+  },
+  map: {
+    'store': 'npm:@ngrx/store@2.0.1'
   }
 });
 
@@ -114,18 +132,17 @@ SystemJS.config({
     'github:*/*.json'
   ],
   map: {
-    '@angular/common': 'npm:@angular/common@2.0.0-rc.2',
-    '@angular/compiler': 'npm:@angular/compiler@2.0.0-rc.2',
-    '@angular/core': 'npm:@angular/core@2.0.0-rc.2',
-    '@angular/forms': 'npm:@angular/forms@0.1.0',
-    '@angular/http': 'npm:@angular/http@2.0.0-rc.2',
-    '@angular/platform-browser': 'npm:@angular/platform-browser@2.0.0-rc.2',
-    '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic@2.0.0-rc.2',
+    '@angular/common': 'npm:@angular/common@2.0.0-rc.3',
+    '@angular/compiler': 'npm:@angular/compiler@2.0.0-rc.3',
+    '@angular/core': 'npm:@angular/core@2.0.0-rc.3',
+    '@angular/forms': 'npm:@angular/forms@0.1.1',
+    '@angular/http': 'npm:@angular/http@2.0.0-rc.3',
+    '@angular/platform-browser': 'npm:@angular/platform-browser@2.0.0-rc.3',
+    '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic@2.0.0-rc.3',
     '@ngrx/core': 'npm:@ngrx/core@1.0.1',
     '@ngrx/router': 'npm:@ngrx/router@1.0.0-beta.1',
     '@ngrx/router-store': 'npm:@ngrx/router-store@0.0.1',
     '@ngrx/store': 'npm:@ngrx/store@2.0.1',
-    'assert': 'github:jspm/nodelibs-assert@0.2.0-alpha',
     'buffer': 'github:jspm/nodelibs-buffer@0.2.0-alpha',
     'child_process': 'github:jspm/nodelibs-child_process@0.2.0-alpha',
     'constants': 'github:jspm/nodelibs-constants@0.2.0-alpha',
@@ -136,16 +153,11 @@ SystemJS.config({
     'immutable': 'npm:immutable@3.8.1',
     'lodash': 'npm:lodash@4.13.1',
     'ng2-translate': 'npm:ng2-translate@2.2.0',
-    'os': 'github:jspm/nodelibs-os@0.2.0-alpha',
-    'path': 'github:jspm/nodelibs-path@0.2.0-alpha',
-    'plugin-typescript': 'github:frankwallis/plugin-typescript@4.0.16',
     'process': 'github:jspm/nodelibs-process@0.2.0-alpha',
     'reflect-metadata': 'npm:reflect-metadata@0.1.3',
     'rxjs': 'npm:rxjs@5.0.0-beta.6',
-    'store': 'npm:@ngrx/store@2.0.1',
     'stream': 'github:jspm/nodelibs-stream@0.2.0-alpha',
     'string_decoder': 'github:jspm/nodelibs-string_decoder@0.2.0-alpha',
-    'util': 'github:jspm/nodelibs-util@0.2.0-alpha',
     'vm': 'github:jspm/nodelibs-vm@0.2.0-alpha',
     'zone.js': 'npm:zone.js@0.6.12'
   },
@@ -323,16 +335,6 @@ SystemJS.config({
       'map': {
         'inherits': 'npm:inherits@2.0.1',
         'readable-stream': 'npm:readable-stream@2.1.4'
-      }
-    },
-    'github:jspm/nodelibs-os@0.2.0-alpha': {
-      'map': {
-        'os-browserify': 'npm:os-browserify@0.2.1'
-      }
-    },
-    'github:frankwallis/plugin-typescript@4.0.16': {
-      'map': {
-        'typescript': 'npm:typescript@1.8.10'
       }
     },
     'npm:query-string@4.2.2': {
