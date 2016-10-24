a40ddc888bc384f770888e41bb24c87d2e678ded
Christian Linne <shadowvampire@web.de>
2016-10-24 15:27:09 +0200

    Use a shared module for common imports and setup stuff.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import {NgModule} from "@angular/core";
 import {HttpModule} from "@angular/http";
+import {BrowserModule} from "@angular/platform-browser";
+import {TranslateModule} from "ng2-translate";
 
 import assign from "lodash/assign";
 
@@ -8,12 +10,16 @@ import {APP_ROUTES, appRoutingProviders} from "./app.routes";
 import {createStoreProvider} from "./app.store";
 import {InputTestModule} from "./inputTest/inputTest.module";
 import {TodosModule} from "./todos/todos.module";
+import {SharedModule} from "./shared/shared.module";
 
 let initialState = {};
 
 const IMPORTS = [
+  BrowserModule, // Should only be imported by the root => every other module should import "CommonModule".
   HttpModule,
   APP_ROUTES,
+  TranslateModule.forRoot(),
+  SharedModule,
   InputTestModule,
   TodosModule,
   createStoreProvider(initialState)
--- src/app/inputTest/inputTest.module.ts
+++ src/app/inputTest/inputTest.module.ts
@@ -1,28 +1,13 @@
-/* @DorianGrey:
- From what I've read:
- https://github.com/angular/angular/issues/10472
- https://github.com/angular/angular/issues/10552
-
- The style below will be the default for >= RC.6 and is now already been used internally.
- Hope they'll reconsider this, since this might screw up most lazy loading techniques...
- */
-
 import {NgModule} from "@angular/core";
-import {BrowserModule} from "@angular/platform-browser";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {InputTestComponent} from "./inputTest.component";
-import {TranslateModule} from "ng2-translate/ng2-translate";
 import {INPUT_TEST_ROUTES} from "./inputTest.routes";
+import {SharedModule} from "../shared/shared.module";
 
 @NgModule({
   imports: [
-    BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
-    TranslateModule.forRoot(),
+    SharedModule,
     INPUT_TEST_ROUTES
   ],
-  exports: [TranslateModule],
   declarations: [InputTestComponent]
 })
 export class InputTestModule {
